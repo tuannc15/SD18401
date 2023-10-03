a89b59eac43e48bb8710fd725dbdf0459d563b69
nctuan2013 <54241848+nctuan2013@users.noreply.github.com>
2023-10-03 19:57:05 +0700

    Khởi tạo dự án

---
+++ TestRepos/TestRepos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+namespace TestRepos;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Kiểm tra git");
+        Console.ReadKey();
+    }
+}
